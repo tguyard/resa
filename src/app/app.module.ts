38607e53cfb947785d7a3e19dcc8c2a4828726ab
Thomas Guyard <thomas.guyard@gmail.com>
2018-04-20 23:17:07 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeaderComponent } from './header/header.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
+import { ReservationComponent } from './reservation/reservation.component';
+
+const appRoutes: Routes = [
+  { path: 'reservations',  component: ReservationComponent },
+  { path: 'configuration', component: ConfigurationComponent },
+  { path: 'dashboard',     component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    ConfigurationComponent,
+    ReservationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+    MatButtonModule,
+    MatToolbarModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
